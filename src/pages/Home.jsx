74ad340f4f2365db4d2bb1a91cf50b8735548211
Harsh Awalegaonkar <harshawalegaonkar653@gmail.com>
2026-04-17 09:15:41 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,47 @@
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 import LoginModal from "../components/LoginModal";
+import { useAuth } from "../components/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const [show, setShow] = useState(false);
+
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
   return (
     <div className="container mt-5 text-center">
       <h1>Welcome</h1>
 
-      <Button onClick={() => setShow(true)}>Login</Button>
+      {user && (
+        <p className="mt-3 fs-5">
+          Hello, <strong>{user.name}</strong> 👋
+        </p>
+      )}
 
-      <LoginModal
-        show={show}
-        handleClose={() => setShow(false)}
-      />
+      {!user ? (
+        <Button onClick={() => setShow(true)}>
+          Login
+        </Button>
+      ) : (
+        <Button
+          variant="danger"
+          onClick={() => {
+            logout();
+            navigate("/");
+          }}
+        >
+          Logout
+        </Button>
+      )}
+
+      {!user && (
+        <LoginModal
+          show={show}
+          handleClose={() => setShow(false)}
+        />
+      )}
     </div>
   );
 }
